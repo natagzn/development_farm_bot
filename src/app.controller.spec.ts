19ab8ef84d0656186ee2d55bb67bd6feb1e296ee
Natalia <n221222122005@gmail.com>
2025-07-25 17:09:13 +0300

    Resolved merge conflicts using ours strategy

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller.ts';
+import { AppController } from './app.controller';
 import { AppService } from './app.service.js';
 
 
--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,9 +1,5 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
 import { TelegramService } from './telegram.service.js';
-=======
-import { TelegramService } from './telegram.service';
->>>>>>> 43bc0fe14a23d5bca752b782407d8a1814c2e8a7
 
 @Module({
   providers: [TelegramService],
